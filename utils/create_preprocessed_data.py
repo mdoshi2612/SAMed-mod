f3cb1fbf3b6883ef8ee5fca5f1c92b4aa2a39720
Manav Doshi <200100094@iitb.ac.in>
2024-02-02 23:19:19 -0800

    Consolidating split and preprocess in one file

--- utils/create_preprocessed_data.py
+++ utils/create_preprocessed_data.py
@@ -4,13 +4,29 @@ import nibabel as nib
 import h5py
 import argparse
 
-def main(ct_scans_folder, effusions_folder, output_folder, save_slice_probability):
+def minmax_scale(data, min_value=-125, max_value=275):
+    # Clip the data
+    clipped_data = np.clip(data, min_value, max_value)
+    
+    # Apply min-max scaling
+    scaled_data = (clipped_data - min_value) / (max_value - min_value)
+    
+    return scaled_data
 
+def main(ct_scans_folder, effusions_folder, output_folder, test, min_value = -125, max_value = 275):
+
+    if(test == True):
+        save_slice_probability = 0.3
+    else:
+        save_slice_probability = 0.7
+
+    # Ensure the output folder exists
     os.makedirs(output_folder, exist_ok=True)
 
     # Iterate through ct_scans folder
     for ct_scan_file in os.listdir(ct_scans_folder):
         if ct_scan_file.endswith(".nii"):
+
             # Construct the paths for ct_scan and effusion files
             ct_scan_path = os.path.join(ct_scans_folder, ct_scan_file)
             base_filename = os.path.splitext(ct_scan_file)[0]
@@ -30,7 +46,7 @@ def main(ct_scans_folder, effusions_folder, output_folder, save_slice_probabilit
                 ct_slice_data = ct_scan_img.get_fdata()[:, :, slice_idx]
                 effusion_slice_data = effusion_img.get_fdata()[:, :, slice_idx]
 
-                # Decide whether to save as .npz or .npy.h5 based on probabilities
+                # Decide whether to save according to train test splits
                 if np.random.rand() <= save_slice_probability:
                     file_extension = ".npz"
 
@@ -38,8 +54,16 @@ def main(ct_scans_folder, effusions_folder, output_folder, save_slice_probabilit
                     output_filename = f"{base_filename}_slice{slice_idx + 1}{file_extension}"
                     output_path = os.path.join(output_folder, output_filename)
 
+                    # Apply min-max scaling to training array
+                    scaled_training = minmax_scale(ct_slice_data, min_value, max_value)
+
+                    # Apply min-max scaling to segmentation array
+                    scaled_segmentation = minmax_scale(effusion_slice_data, 0, 1)
+
                     # Save the data based on file extension
-                    np.savez(output_path, image=ct_slice_data, label=effusion_slice_data)
+                    np.savez(output_path, image=scaled_training, label=scaled_segmentation)
+            
+                    print(f"{output_filename} was normalized and saved.")
 
     # Perform any additional operations as needed
     print("Processing complete.")
@@ -49,7 +73,9 @@ if __name__ == "__main__":
     parser.add_argument("--ct_scans_folder", type=str, required=True, help="Path to the folder containing CT scans.")
     parser.add_argument("--effusions_folder", type=str, required=True, help="Path to the folder containing effusion masks.")
     parser.add_argument("--output_folder", type=str, required=True, help="Path to the output folder.")
-    parser.add_argument("--save_slice_probability", type=float, default=0.7, help="Probability of saving each slice.")
+    parser.add_argument("--test", action = 'store_true', help="Probability of saving each slice.")
+    parser.add_argument("--min_value", type=float, default=-125, help="Minimum value for scaling.")
+    parser.add_argument("--max_value", type=float, default=275, help="Maximum value for scaling.")
 
     args = parser.parse_args()
-    main(args.ct_scans_folder, args.effusions_folder, args.output_folder, args.save_slice_probability)
+    main(args.ct_scans_folder, args.effusions_folder, args.output_folder, args.test, args.min_value, args.max_value)
